4dfa465dc26f9879c2c557c3e4cad74d11048e10
Chris Quattrocchi and Tyler Bussanich <tyler@>
2017-10-24 16:20:02 -0700

    useless tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
       var workTransportationMode = $(this).val();
       $("#work-responses").append(workTransportationMode + "<br>");
     });//end each fxn
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $("#fun-responses").append(funTransportationMode + "<br>");
+    });//end each fxn
     $("#transportation_survey").hide();
   });//end submit fxn
 });//end ready fxn
